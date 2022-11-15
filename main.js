797de4231c69435317f1a5c7f7a4066545e2a85e
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-11-15 09:50:58 +0500

    filnal answer(test)

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const fs = require('fs')
+const crypto = require('crypto')
+
+const filnalHex = []
+let resStr =""
 
 const filenames = fs.readdirSync('./data/')
-console.log(filenames)
+filenames.forEach(fName => {
+    const buff = fs.readFileSync(`./data/${fName}`)
+    const hash = crypto.createHash("sha3-256").update(buff).digest("hex")
+    filnalHex.push(hash)
+})
+
+filnalHex.sort().join('')
+console.log(filnalHex)
+
+filnalHex.forEach(hex=>{
+    resStr+=hex
+})
+
+resStr+="makhmudov@tuta.io"
+resStr = crypto.createHash("sha3-256").digest("hex")
+console.log(resStr)
